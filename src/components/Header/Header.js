59f9177a14b400a7b91344d1463ed0d0a00a1cfc
AIA <adashik585@gmail.com>
2022-03-26 03:34:35 +0600

    Header component addded

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>Gaming Accessories Buy</h1>
+            <h2>Choose 4 Items</h2>
+        </header>
+    );
+};
+
+export default Header;
